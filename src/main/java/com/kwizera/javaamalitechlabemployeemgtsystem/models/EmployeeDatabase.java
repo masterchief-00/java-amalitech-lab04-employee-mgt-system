e4a03c889dc1574d6e2c3970acaf06103da493d5
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-04-16 19:23:50 +0200

    added filtering and searching methods to the employee db class

--- src/main/java/com/kwizera/javaamalitechlabemployeemgtsystem/models/EmployeeDatabase.java
+++ src/main/java/com/kwizera/javaamalitechlabemployeemgtsystem/models/EmployeeDatabase.java
@@ -2,7 +2,9 @@ package com.kwizera.javaamalitechlabemployeemgtsystem.models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EmployeeDatabase<T> {
     private Map<T, Employee<T>> employeeMap;
@@ -51,7 +53,39 @@ public class EmployeeDatabase<T> {
         }
     }
 
-    public ArrayList<Employee<T>> getAllEmployees() {
-        return new ArrayList<>(employeeMap.values());
+    public List<Employee<T>> getAllEmployees() {
+        List<Employee<T>> allEmployees = new ArrayList<>();
+        return allEmployees;
+    }
+
+    public List<Employee<T>> getEmployeesByDepartment(String department) {
+        return employeeMap.values()
+                .stream()
+                .filter(e -> e.getDepartment().equalsIgnoreCase(department))
+                .collect(Collectors.toList());
+    }
+
+    public List<Employee<T>> getEmployeeBySearchTerm(String searchTerm) {
+        return employeeMap.values()
+                .stream()
+                .filter(e -> e.getName().contains(searchTerm.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Employee<T>> getEmployeeBySalaryRange(double minSalary, double maxSalary) {
+        return employeeMap
+                .values()
+                .stream()
+                .filter(e -> e.getSalary() >= minSalary && e.getSalary() <= maxSalary)
+                .collect(Collectors.toList());
+    }
+
+    // note to self: iterator to be used from the method invoking this one
+    public List<Employee<T>> getEmployeeByPerformanceRating(double minRating) {
+        return employeeMap
+                .values()
+                .stream()
+                .filter(e -> e.getPerformanceRating() >= minRating)
+                .collect(Collectors.toList());
     }
 }
